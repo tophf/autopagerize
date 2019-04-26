b71fe3aca09edf9043f71eb38ae8a74072265efa
tophf <tophf@gmx.com>
2019-04-26 16:43:10 +0300

    add import/export in options

--- ui/options.js
+++ ui/options.js
@@ -23,6 +23,9 @@ Promise.all([
 
   $.btnSave.onclick = save;
   $.btnUpdate.onclick = update;
+  $.btnImport.onclick = importSettings;
+  $.btnExport.onclick = exportSettings;
+  $.backup.oninput = ({target: el}) => ($.importWrapper.disabled = !el.value.trim());
   addEventListener('input', onChange, {passive: true});
   addEventListener('change', onChange, {passive: true});
 });
@@ -45,18 +48,15 @@ function renderSiteinfoStats(numRules, date) {
 
 async function save() {
   const settings = await getSettings();
-  const rules = collectRules();
+  const ss = collectSettings();
   const changed =
-    $.excludes.value.trim() !== arrayOrDummy(settings.excludes).join('\n') ||
-    $.showStatus.checked !== settings.showStatus ||
-    !rulesEqual(rules, settings.rules);
+    ss.excludes.join('\n') !== arrayOrDummy(settings.excludes).join('\n') ||
+    ss.showStatus !== settings.showStatus ||
+    !rulesEqual(ss.rules, settings.rules);
   if (changed) {
-    settings.rules = rules;
-    settings.excludes = $.excludes.value.trim().split(/\s+/);
-    settings.showStatus = $.showStatus.checked;
-    $.excludes.savedValue = settings.excludes.join('\n');
-    $.showStatus.savedValue = settings.showStatus;
-    inBG.writeSettings(settings);
+    $.excludes.savedValue = ss.excludes.join('\n');
+    $.showStatus.savedValue = ss.showStatus;
+    inBG.writeSettings({...settings, ...ss});
     changedElements.clear();
     $.btnSaveWrapper.hidden = true;
   }
@@ -82,7 +82,58 @@ async function update() {
   btn.disabled = false;
 }
 
+function collectSettings() {
+  return {
+    rules: collectRules(),
+    excludes: $.excludes.value.trim().split(/\s+/),
+    showStatus: $.showStatus.checked,
+  };
+}
+
+async function importSettings() {
+  let imported;
+  try {
+    imported = JSON.parse($.backup.value);
+    $.importError.hidden = true;
+  } catch (e) {
+    $.importError.textContent = String(e);
+    $.importError.hidden = false;
+    return;
+  }
+  const defaultSettings = {
+    showStatus: true,
+    enabled: await getSettings().enabled,
+    rules: [],
+    excludes: [],
+  };
+  const ovr = $.overwriteSettings.checked;
+  const settings = ovr ? defaultSettings : collectSettings();
+  for (const [k, ref] of Object.entries(defaultSettings)) {
+    const v = imported[k];
+    settings[k] =
+      ref === true || ref === false ? Boolean(v) :
+        typeof ref === 'string' ? String(v) :
+          typeof ref === 'number' ? Number(v) :
+            Array.isArray(ref) && Array.isArray(v) ? [...settings[k], ...v] :
+              v;
+  }
+  await inBG.writeSettings(settings);
+  $.rules.textContent = '';
+  loadRules(settings.rules);
+  renderSettings(settings);
+}
+
+function exportSettings() {
+  $.backup.focus();
+  $.backup.select();
+  document.execCommand('insertText', false, JSON.stringify(collectSettings(), null, '  '));
+  $.backup.select();
+  $.importError.hidden = true;
+}
+
 function onChange({target: el}) {
+  if (el.closest('.ignore-changes'))
+    return;
   const value = el[el.type === 'checkbox' ? 'checked' : 'value'];
   const changed = value !== el.savedValue;
   if (changed)
