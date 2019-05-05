bf45b058628147550106632024a91b804a4c2a59
tophf <tophf@gmx.com>
2019-05-05 16:30:38 +0300

    convert <foo> in i18n messages to <b>foo</b> HTML

--- util/locale.js
+++ util/locale.js
@@ -2,6 +2,8 @@ export {
   i18n,
 };
 
+const TAG = /(<\S.*?>)/;
+
 onMutation([{addedNodes: document.querySelectorAll('[tl]')}]);
 
 if (document.readyState === 'loading') {
@@ -16,8 +18,11 @@ function onMutation(mutations, observer) {
   for (const m of mutations) {
     for (const node of m.addedNodes) {
       if (!observer || node.tagName && node.hasAttribute('tl')) {
-        const textNode = node.firstChild;
-        textNode.nodeValue = i18n(textNode.nodeValue.trim());
+        const str = i18n(node.textContent.trim());
+        if (TAG.test(str))
+          renderTags(node, str);
+        else
+          node.firstChild.nodeValue = str;
       }
     }
   }
@@ -25,4 +30,18 @@ function onMutation(mutations, observer) {
 
 function i18n(...args) {
   return chrome.i18n.getMessage(...args);
+}
+
+function renderTags(el, str) {
+  el.textContent = '';
+  el.append(...str.split(TAG).map(renderTagOrString));
+}
+
+function renderTagOrString(str) {
+  if (str.startsWith('<') && str.endsWith('>')) {
+    const el = document.createElement('b');
+    el.textContent = str.slice(1, -1);
+    return el;
+  } else
+    return str;
 }
