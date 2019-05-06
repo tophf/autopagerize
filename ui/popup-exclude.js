62b60ea9b7b7060231ac2a0f7177daeb647583fb
tophf <tophf@gmx.com>
2019-05-06 17:14:25 +0300

    process all affected tabs when excluding

--- ui/popup-exclude.js
+++ ui/popup-exclude.js
@@ -18,22 +18,24 @@ async function exclude(e) {
   e.preventDefault();
 
   const ss = await getSettings();
+  const pattern = e.target.title;
   const exclusions = arrayOrDummy(ss.exclusions);
-  if (!exclusions.includes(e.target.title)) {
-    exclusions.push(e.target.title);
+  if (!exclusions.includes(pattern)) {
+    exclusions.push(pattern);
     inBG.writeSettings({exclusions});
   }
 
-  const tabId = popup.tab.id;
   let path = chrome.runtime.getManifest().browser_action.default_popup;
   if (!path.startsWith('/'))
     path = '/' + path;
 
-  await Promise.all([
-    inBG.setIcon({tabId, type: 'off'}),
-    executeScript(tabId, () => typeof run === 'function' && window.run({terminate: true})),
-    new Promise(resolve => chrome.browserAction.setPopup({tabId, popup: path}, resolve)),
-  ]);
+  for (const {id: tabId} of await findExcludedTabs(pattern)) {
+    await Promise.all([
+      inBG.setIcon({tabId, type: 'off'}),
+      executeScript(tabId, terminateContentScript),
+      new Promise(resolve => chrome.browserAction.setPopup({tabId, popup: path}, resolve)),
+    ]);
+  }
 
   location.assign(path);
 }
@@ -48,4 +50,25 @@ function updateTitles() {
           type === 'domain' ? new URL(url).origin + '/*' : '';
     el.onclick = exclude;
   }
+}
+
+function findExcludedTabs(pattern) {
+  const isPrefix = pattern.endsWith('*');
+  const url = isPrefix ? pattern.slice(0, -1) : pattern;
+  // the API can't query #hash and may return tabs with a #different-hash
+  // so we need to get all tabs with the same base URL and then filter the results
+  const hashlessUrl = pattern.split('#', 1)[0] + (isPrefix ? '*' : '');
+  return new Promise(resolve =>
+    chrome.tabs.query({url: hashlessUrl}, tabs => {
+      tabs = isPrefix
+        ? tabs.filter(t => t.url.startsWith(url))
+        : tabs.filter(t => t.url === url);
+      resolve(tabs);
+    }));
+}
+
+function terminateContentScript() {
+  window.launched = false;
+  if (typeof run === 'function')
+    window.run({terminate: true});
 }
