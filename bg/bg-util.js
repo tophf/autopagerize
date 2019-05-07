9b6f52310f3e0c5b1c770118b835008487120aa6
tophf <tophf@gmx.com>
2019-05-07 10:13:33 +0300

    simplify isUrlExcluded

--- bg/bg-util.js
+++ bg/bg-util.js
@@ -1,4 +1,9 @@
-// inline 'export' keyword is used since everything in this module is expected to be exported
+export {
+  calcRuleKey,
+  calcUrlCacheKey,
+  isUrlExcluded,
+  ruleKeyToUrl,
+};
 
 export const utf8encoder = new TextEncoder();
 export const utf8decoder = new TextDecoder();
@@ -9,7 +14,7 @@ import {arrayOrDummy} from '/util/common.js';
 import {settings} from './bg.js';
 
 // N.B. requires 'settings' to be already loaded when no 'exclusions' were supplied
-export function isUrlExcluded(url, exclusions) {
+function isUrlExcluded(url, exclusions) {
   if (url.startsWith('https://mail.google.com/') ||
       url.startsWith('http://b.hatena.ne.jp/') ||
       url.startsWith('https://www.facebook.com/plugins/like.php') ||
@@ -26,37 +31,37 @@ export function isUrlExcluded(url, exclusions) {
       if (i === entry.length - 1 && url.startsWith(entry.slice(0, -1)))
         return true;
     }
-    let rx = str2rx.get(entry);
-    if (rx === null)
-      continue;
-    if (!rx) {
-      try {
-        const rxStr = isRegexp
-          ? entry.slice(1, -1)
-          : '^' +
-            entry.replace(/([-()[\]{}+?.$^|\\])/g, '\\$1')
-              .replace(/\x08/g, '\\x08')
-              .replace(/\*/g, '.*') +
-            '$';
-        rx = RegExp(rxStr);
-        str2rx.set(entry, rx);
-      } catch (e) {
-        str2rx.set(entry, null);
-        continue;
-      }
-    }
-    if (rx.test(url))
+    const rx = str2rx.get(entry);
+    if (rx !== null && (rx || compileExclusion(entry, isRegexp)).test(url))
       return true;
   }
 }
 
-export async function calcUrlCacheKey(url) {
+function compileExclusion(str, isRegexp) {
+  let rx;
+  try {
+    const rxStr = isRegexp
+      ? str.slice(1, -1)
+      : '^' +
+        str.replace(/([-()[\]{}+?.$^|\\])/g, '\\$1')
+          .replace(/\x08/g, '\\x08')
+          .replace(/\*/g, '.*') +
+        '$';
+    rx = RegExp(rxStr);
+  } catch (e) {
+    rx = null;
+  }
+  str2rx.set(str, rx);
+  return rx;
+}
+
+async function calcUrlCacheKey(url) {
   const bytes = utf8encoder.encode(url);
   const hash = await crypto.subtle.digest('SHA-256', bytes);
   return new Uint8Array(hash).slice(0, 16);
 }
 
-export function calcRuleKey(rule) {
+function calcRuleKey(rule) {
   const url = utf8encoder.encode(rule.url);
   const key = new Uint8Array(url.length + 4);
   new DataView(key.buffer).setUint32(0, rule.id, true);
@@ -64,6 +69,6 @@ export function calcRuleKey(rule) {
   return key;
 }
 
-export function ruleKeyToUrl(key) {
+function ruleKeyToUrl(key) {
   return utf8decoder.decode(key.slice(4));
 }
