2106d79e2e33dc977352eb7371e54b6aead73a8f
tophf <tophf@gmx.com>
2020-01-04 21:56:41 +0300

    1.0.4 fix and generalize google thumbnails fixer

--- content/fix-google.js
+++ content/fix-google.js
@@ -4,19 +4,19 @@
 window.run({
   filterName: 'fix-google',
   filter: doc => {
-    const script = [...doc.getElementsByTagName('script')]
-      .find(el => el.text.includes('_setImagesSrc'));
-    if (!script)
-      return;
-    const re = /["'](data:image.*?)["'].*?\[(.*?)]\W*_setImagesSrc/g;
-    let m;
-    while ((m = re.exec(script.text))) {
-      const [, dataUrl, ids] = m;
-      for (const id of ids.match(/[^,'"]+/g) || []) {
-        const el = doc.getElementById(id);
-        if (el)
-          el.src = dataUrl.replace(/\\x([0-9a-f]{2})/gi, (_, code) =>
-            String.fromCharCode(parseInt(code, 16)));
+    const re = /["'](data:image[^"']+)["'].*?\[(.*?)]\W*_setImagesSrc/g;
+    for (const {text} of doc.getElementsByTagName('script')) {
+      if (!text.includes('_setImagesSrc'))
+        continue;
+      let m;
+      while ((m = re.exec(text))) {
+        const [, dataUrl, ids] = m;
+        for (const id of ids.match(/[^,'"]+/g) || []) {
+          const el = doc.getElementById(id);
+          if (el)
+            el.src = dataUrl.replace(/\\x([0-9a-f]{2})/gi, (_, code) =>
+              String.fromCharCode(parseInt(code, 16)));
+        }
       }
     }
   },
