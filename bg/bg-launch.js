bc14b5374d15c3b37759260f8c0301f548d32c0c
tophf <tophf@gmx.com>
2021-09-13 14:52:26 +0300

    simplify poke()

--- bg/bg-launch.js
+++ bg/bg-launch.js
@@ -10,22 +10,22 @@ import {settings} from './bg.js';
  * true if there's an applicable rule (used by lastTry='genericRules' mode)
  */
 async function launch({tabId, url, rules, lastTry}) {
-  if (!await poke(tabId).checkDeps()) {
+  if (!await execScript(tabId, contentCheckDeps)) {
     // no deps while retrying means the tab got navigated away
     // and already being handled in another event
     if (lastTry === 'setTimeout')
       return;
-    await poke(tabId, {file: '/content/xpather.js'});
+    await execScript(tabId, {file: '/content/xpather.js'});
   }
 
-  const rr = await poke(tabId).checkRules(rules, !lastTry && RETRY_TIMEOUT) || {};
+  const rr = await execScript(tabId, contentCheckRules, rules, !lastTry && RETRY_TIMEOUT) || {};
   if (!rr.hasRule && !lastTry)
     return new Promise(r => setTimeout(retry, RETRY_TIMEOUT, r, arguments[0]));
   if (!rr.hasRule || lastTry === 'genericRules')
     return rr.hasRule;
 
   if (!rr.hasRun)
-    await poke(tabId, {file: '/content/pager.js'});
+    await execScript(tabId, {file: '/content/pager.js'});
 
   if (url.includes('google.') &&
       /^https?:\/\/(www\.)?google(\.com?)?(\.\w\w)?\/(search\?|.*?[?&#]q=[^&]+)/.test(url))
@@ -43,56 +43,42 @@ async function launch({tabId, url, rules, lastTry}) {
     const def = DEFAULTS[name];
     ss[name] = typeof def === 'string' ? v || def : v;
   }
-  await poke(tabId).doRun(ss);
+  await execScript(tabId, contentDoRun, ss);
 }
 
 function retry(resolve, cfg) {
   launch({...cfg, lastTry: 'setTimeout'}).then(resolve);
 }
 
-// declare as anonymous functions for proper stringification in executeScript
-const CONTENT_SCRIPT_CODE = {
-  checkDeps: function () {
-    return typeof (window.xpather || {}).getMatchingRule === 'function';
-  },
-  checkRules: function (rules, retryTimeout) {
-    if (typeof (window.xpather || {}).getMatchingRule !== 'function')
-      return;
-    const r = window.xpather.getMatchingRule(rules);
-    if (r) {
-      clearTimeout(window.retryTimer);
-      window.retryTimer = null;
-      window.rules = rules;
-      window.matchedRule = r;
-      return {
-        hasRule: true,
-        hasRun: typeof run === 'function',
-      };
-    } else {
-      window.retryTimer = setTimeout(() => {
-        if (typeof run !== 'function')
-          delete window.xpather;
-      }, retryTimeout * 1.5);
-    }
-  },
-  doRun: function (settings) {
-    // eslint-disable-next-line no-undef
-    window.run({rules, matchedRule, settings});
-    window.launched = true;
-    delete window.rules;
-    delete window.matchedRule;
-  },
-};
+function contentCheckDeps() {
+  return typeof (window.xpather || {}).getMatchingRule === 'function';
+}
 
-const POKE_HANDLER = {
-  get(cfg, name) {
-    return (...params) =>
-      execScript(cfg.tabId, CONTENT_SCRIPT_CODE[name], ...params);
-  },
-};
+function contentCheckRules(rules, retryTimeout) {
+  if (typeof (window.xpather || {}).getMatchingRule !== 'function')
+    return;
+  const r = window.xpather.getMatchingRule(rules);
+  if (r) {
+    clearTimeout(window.retryTimer);
+    window.retryTimer = null;
+    window.rules = rules;
+    window.matchedRule = r;
+    return {
+      hasRule: true,
+      hasRun: typeof run === 'function',
+    };
+  } else {
+    window.retryTimer = setTimeout(() => {
+      if (typeof run !== 'function')
+        delete window.xpather;
+    }, retryTimeout * 1.5);
+  }
+}
 
-function poke(tabId, options) {
-  return options
-    ? execScript(tabId, options)
-    : new Proxy({tabId}, POKE_HANDLER);
+function contentDoRun(settings) {
+  // eslint-disable-next-line no-undef
+  run({rules, matchedRule, settings});
+  window.launched = true;
+  delete window.rules;
+  delete window.matchedRule;
 }
